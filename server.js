261d5da6e38250e28cb21ce3cbdf51b9d74a9cb1
sahilbest999 <sahilpshahane123@gmail.com>
2023-02-01 12:34:38 +0530

    Fix: Remove unused environment variables & mailjet email client

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const {
   getTotalUserEntries,
 } = require('./lib/database')
 const { CUSTOM_ERROR } = require('./lib/errors')
-const validator = require('validator')
 
 connectToDB()
 
@@ -31,27 +30,6 @@ app.use(morgan('tiny'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const validatePhoneno = (phoneno) => {
-  console.log('Validate Phone no. Task Received')
-
-  // REGEX + Named Capturing  | requires EcmaScript / JavaScript
-  // ^(?<country_code>((\+\d{1,}\s+)|()))(?<phoneno>(\d{10}))$
-  const phoneDetails = String(phoneno)
-    .trim()
-    .match(/^(?<countryCode>((\+\d{1,}\s+)|()))(?<number>(\d{10}))$/)
-
-  if (!phoneDetails) return null
-  return {
-    countryCode: phoneDetails.groups['countryCode'],
-    number: Number(phoneDetails.groups['number']),
-  }
-}
-
-const validateEmail = (email) => {
-  console.log('Validate Email Task Received')
-  return validator.isEmail(email)
-}
-
 app.post('/user-form', async (req, res) => {
   const { name, dob, email, phoneNo } = req.body
 
@@ -61,11 +39,8 @@ app.post('/user-form', async (req, res) => {
         'Please provide valid name, date of birth, email, phoneNo'
       )
 
-    // const phoneDetails = await validatePhoneNoTask.applyAsync([phoneNo]).get()
-    // const isEmailValid = await validateEmailTask.applyAsync([email]).get()
-
-    const phoneDetails = validatePhoneno(phoneNo)
-    const isEmailValid = validateEmail(email)
+    const phoneDetails = await validatePhoneNoTask.applyAsync([phoneNo]).get()
+    const isEmailValid = await validateEmailTask.applyAsync([email]).get()
 
     if (!phoneDetails)
       throw new CUSTOM_ERROR('Provided Phone no. is invalid', 400)
